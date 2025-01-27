267365e27090e1b62b5003a2ca6a1592629fa224
Crimsonet <crimsonet@tuta.io>
2025-01-27 15:26:06 +0800

    Jan 26,2025:1525

---
+++ analyse/calculator.go
@@ -0,0 +1,14 @@
+/**
+ *	@Encode : UTF-8
+ *	@Author : Crimsonet
+ *	@Repo   : maple-layout
+ *	@File   : calculator
+ *	@Time   : 1/27/25
+ *	@IDE    : GoLand
+ */
+
+package analyse
+
+func Analysis() {
+	
+}
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Crimsonet/maple-layout/editor"
+	"github.com/Crimsonet/maple-layout/factors"
+)
+
 func main() {
-	println("Hello Maple!")
+
+	//如果想看自然碼原始輸出結果
+	//或者想看加權平均數輸出結果
+	//的話，這裏改一下就可以了。
+	whetherPrintRawNaturalCode := false
+	whetherPrintProNaturalCode := false
+
+	if whetherPrintRawNaturalCode == true {
+		fileName := "factors/chinese.json"
+		letterCountRaw := factors.ProcessJSONFile(fileName)
+		if letterCountRaw != nil {
+			fmt.Println("自然碼數據統計結果:")
+			for letter, count := range letterCountRaw {
+				fmt.Printf("%c: %d\n", letter, count)
+			}
+		}
+	}
+	if whetherPrintProNaturalCode == true {
+		chineseFilePath := "./editor/stats/chinese.json"
+		englishFilePath := "./editor/stats/english.json"
+		editor.Processor(chineseFilePath, englishFilePath)
+	}
+
 }
